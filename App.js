5a3bc81e6dccffdcacb1f240dc0630262904b357
pablormacia <pr.macia@gmail.com>
2023-05-24 03:42:59 -0300

    Clase 4 - Eventos&listas optimizadas

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, Modal, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [textItem, setTextItem] = useState('')
+  const [itemList,setItemList] = useState([])
+  const [itemSelected, setItemSelected] = useState({})
+  const [modalVisible,setModalVisible] = useState(false)
+
+  const onHandleChangeText = (t) =>  setTextItem(t)
+
+  const addItem = () => {
+    setItemList(currentItems => [...currentItems,{id: Math.random().toString(), value:textItem}])
+    setTextItem('')
+    console.log(itemList)
+  }
+
+  const onHandleOpenModal = () => {
+    setModalVisible(!modalVisible)
+  }
+
+  const onHandleDelete = (id) => {
+    console.log(id)
+  }
+
+  const renderItem = ({item}) => (
+      <TouchableOpacity onPress={onHandleOpenModal}><Text>{item.value}</Text></TouchableOpacity>
+  )
+  
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Item de lista' style={styles.input}/>
-        <Button title="Add" />
+        <TextInput placeholder='Item de lista' style={styles.input} onChangeText={onHandleChangeText} value={textItem} />
+        <Button title="Add" onPress={addItem} />
       </View>
-      <View></View>
+      <View>
+        {/* {itemList.map(item=><Text>{item.value}</Text>)} */}
+        <FlatList
+          data={itemList}
+          renderItem = {renderItem}
+          keyExtractor={item=>item.id}
+        />
+      </View>
+      <Modal visible={modalVisible} animationType='slide'>
+        <View style={styles.modalTitle}>
+          <Text>Mi modal</Text>
+        </View>
+        <View style={styles.modalMessage}>
+          <Text>¿Estás seguro que deseas borrar?</Text>
+        </View>
+        <View style={styles.modalMessage}>
+          <Text style={styles.modalItem}>{itemSelected.value}</Text>
+        </View>
+        <View style={styles.modalButton}>
+          <Button onPress={()=>onHandleDelete(itemSelected.id)} title="Confirmar" />
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -25,5 +72,21 @@ const styles = StyleSheet.create({
     width: 200,
     borderBottomColor: 'black',
     borderBottomWidth: 1,
+  },
+  modalTitle:{
+    backgroundColor: "#ccc",
+    color: 'white',
+    fontSize: 18
+  },
+  modalMessage: {
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalButton: {
+    marginTop: 15
+  },
+  modalItem: {
+    fontSize: 30
   }
 });
